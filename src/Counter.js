ebfd10d7eb5a10e78524cd7a59775b7221e1ad19
Val8ef <lerileri.lers@gmail.com>
2025-02-20 12:39:55 +0300

    Counter component implemented

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,22 @@
 import { Button, Card } from 'react-bootstrap';
+import { useState } from 'react';
 
 function Counter() {
-    const handleIncrement = () => console.log('Increment');
-    const handleDecrement = () => console.log('Decrement');
-    const handleReset = () => console.log('Reset');
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => setCount(count + 1);
+    const handleDecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
+    const handleReset = () => setCount(0);
 
     return (
         <Card>
             <Card.Body>
                 <h2>Счетчик</h2>
-                <p>Текущее значение: 0</p>
+                <p>Текущее значение: {count}</p>
                 <Button onClick={handleIncrement} variant="primary">+</Button>{' '}
                 <Button onClick={handleDecrement} variant="secondary">-</Button>{' '}
                 <Button onClick={handleReset} variant="warning">Сброс</Button>
